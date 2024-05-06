8fcb84422b30be78363044512a0d0e462224be63
Nabila Alawiyah <118095708+nabilalwyh@users.noreply.github.com>
2024-05-06 22:23:36 +0700

    Add files via upload

---
+++ funda-web/pert3/no2.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b, c int
+
+	a = 50
+	b = 20
+	c = 30
+
+	x := a + b - c
+	y := a * b
+	z := a / b
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
---
+++ funda-web/pert3/no5.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai int
+	nilai = 90
+
+	switch nilai {
+	case 100:
+		fmt.Println("Excellent!")
+		break
+	case 90:
+		fmt.Println("Good Job!")
+		break
+	case 80:
+		fmt.Println("Better!")
+		break
+	default:
+		fmt.Println("Sorry, You Fail!")
+	}
+
+}
